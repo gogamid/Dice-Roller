a816d3ab34e5c763f2a92c5aec20fb345224ff1a
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-08 20:12:26 +0200

    - display the correct dice image based on dice roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -22,7 +23,8 @@ class MainActivity : AppCompatActivity() {
     val roll2 = dice.roll()
     val dice1: ImageView = findViewById(R.id.dice1_image)
     val dice2: ImageView = findViewById(R.id.dice2_image)
-    dice1.setImageResource(R.drawable.dice_1)
-    dice2.setImageResource(R.drawable.dice_1)
+    val drawables = R.drawable.dice_1..R.drawable.dice_6
+    dice1.setImageResource(drawables.elementAt(roll1 - 1))
+    dice2.setImageResource(drawables.elementAt(roll2 - 1))
   }
 }
